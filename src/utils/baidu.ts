2573cc9a81e1f23745dd86347de6c5146ce6384c
Hooray Hu <304327508@qq.com>
2023-06-04 22:13:03 +0800

    更新百度统计，更新 github actions

--- src/utils/baidu.ts
+++ src/utils/baidu.ts
@@ -7,7 +7,7 @@ if (!import.meta.env.DEV) {
   }]);
   (function () {
     const hm = document.createElement('script')
-    hm.src = 'https://hm.baidu.com/hm.js?506cda33993a1a0af70a34ad844b9663'
+    hm.src = 'https://hm.baidu.com/hm.js?f74bb3cde8d979af3d9bd804b24070a5'
     const s = document.getElementsByTagName('script')[0]
     s.parentNode?.insertBefore(hm, s)
   })()
