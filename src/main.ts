dea72e7c9d5f85b63b47e30320c9cbcae82ffcf9
Hooray Hu <304327508@qq.com>
2023-11-02 01:09:28 +0800

    refactor: 移除 Element Plus 自定义主题配置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import '@/utils/system.copyright'
 
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
 import App from './App.vue'
 import pinia from './store'
 import router from './router'
