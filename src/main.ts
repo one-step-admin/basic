633ffd3683460e90921f4335eaaa7baa4987a7f6
Hooray Hu <304327508@qq.com>
2023-11-02 01:09:55 +0800

    移除 Element Plus 自定义主题配置

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import '@/utils/baidu'
 import '@/utils/system.copyright'
 
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
 import App from './App.vue'
 import pinia from './store'
 import router from './router'
