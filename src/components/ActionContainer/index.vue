143d0e68ec052c264c92c6b6fde3edeadc59148c
Hooray Hu <304327508@qq.com>
2023-12-19 23:43:03 +0800

    <ActionContainer> 组件内滚动条组件替换

--- src/components/ActionContainer/index.vue
+++ src/components/ActionContainer/index.vue
@@ -1,9 +1,13 @@
+<script setup lang="ts">
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-vue'
+</script>
+
 <template>
   <div class="action-container absolute left-0 top-0 h-full w-full flex flex-col">
     <div class="flex-1 of-hidden">
-      <ElScrollbar>
+      <OverlayScrollbarsComponent :options="{ scrollbars: { autoHide: 'leave', autoHideDelay: 300 } }" defer class="h-full">
         <slot />
-      </ElScrollbar>
+      </OverlayScrollbarsComponent>
     </div>
     <div v-if="$slots.action" class="z-1 flex-center border-t border-t-(stone-2 solid) bg-[var(--g-app-bg)] p-4 transition-background-color-300 dark:border-t-stone-8">
       <slot name="action" />
