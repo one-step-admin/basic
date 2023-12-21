3e11aee3a968c7bb1e5cf3bcb5496ac9efe213ec
Hooray Hu <304327508@qq.com>
2023-12-21 22:04:02 +0800

    修改示例

--- src/views/windows/WindowExampleAdd/index.vue
+++ src/views/windows/WindowExampleAdd/index.vue
@@ -4,8 +4,8 @@ const appWindow = useWindow()
 
 <template>
   <PageMain>
-    <ElButton @click="appWindow.add({ title: '新窗口', name: 'WindowExampleNewWindow' })">
+    <HButton @click="appWindow.add({ title: '新窗口', name: 'WindowExampleNewWindow' })">
       打开一个新窗口（该窗口非导航栏菜单）
-    </ElButton>
+    </HButton>
   </PageMain>
 </template>
