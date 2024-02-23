a0f86102c5cad4ed054adc84d9ea1dc5d21a8d8e
Hooray Hu <304327508@qq.com>
2024-02-24 02:21:31 +0800

    修复 iconify 演示页面弹窗被遮挡

--- src/views/windows/IconExampleIconify/index.vue
+++ src/views/windows/IconExampleIconify/index.vue
@@ -40,38 +40,40 @@ function showInfo(iconName: string) {
         </ElCol>
       </ElRow>
     </PageMain>
-    <ElDialog v-model="dialog.visible" :show-close="false" width="600px">
-      <ElRow type="flex">
-        <ElCol style="max-width: 100px;">
-          <div class="icon-box">
-            <SvgIcon :name="dialog.iconName" />
-          </div>
-        </ElCol>
-        <ElCol style="flex: 1;">
-          <div class="icon-info">
-            <div class="title">
-              {{ dialog.iconName }}
+    <ElDialog v-model="dialog.visible" :show-close="false" width="600px" append-to-body>
+      <div class="iconify-dialog">
+        <ElRow type="flex">
+          <ElCol style="max-width: 100px;">
+            <div class="icon-box">
+              <SvgIcon :name="dialog.iconName" />
             </div>
-            <ElInput v-model="code" readonly>
-              <template #append>
-                <ElButton type="primary" @click="copy(code)">
-                  复制
-                </ElButton>
-              </template>
-            </ElInput>
-          </div>
-        </ElCol>
-      </ElRow>
-      <div class="more-info">
-        <ElDivider content-position="left">
-          结合 Element Plus 使用场景
-        </ElDivider>
-        <ElButton>
-          <template #icon>
-            <SvgIcon :name="dialog.iconName" />
-          </template>
-          Icon 按钮
-        </ElButton>
+          </ElCol>
+          <ElCol style="flex: 1;">
+            <div class="icon-info">
+              <div class="title">
+                {{ dialog.iconName }}
+              </div>
+              <ElInput v-model="code" readonly>
+                <template #append>
+                  <ElButton type="primary" @click="copy(code)">
+                    复制
+                  </ElButton>
+                </template>
+              </ElInput>
+            </div>
+          </ElCol>
+        </ElRow>
+        <div class="more-info">
+          <ElDivider content-position="left">
+            结合 Element Plus 使用场景
+          </ElDivider>
+          <ElButton>
+            <template #icon>
+              <SvgIcon :name="dialog.iconName" />
+            </template>
+            Icon 按钮
+          </ElButton>
+        </div>
       </div>
     </ElDialog>
   </div>
@@ -92,36 +94,32 @@ function showInfo(iconName: string) {
   }
 }
 
-:deep(.el-dialog) {
-  .el-dialog__body {
-    padding: 0 30px 30px;
+.iconify-dialog {
+  padding: 0 16px 16px;
+}
+
+.icon-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 80px;
+  border: 1px solid #e0e8ef;
+
+  i {
+    font-size: 60px;
   }
+}
 
-  .icon-box {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 80px;
-    height: 80px;
-    border: 1px solid #e0e8ef;
+.icon-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
 
-    i {
-      font-size: 60px;
-      color: #333;
-    }
-  }
-
-  .icon-info {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 100%;
-
-    .title {
-      font-size: 24px;
-      font-weight: bold;
-      color: #333;
-    }
+  .title {
+    font-size: 24px;
+    font-weight: bold;
   }
 }
 </style>
